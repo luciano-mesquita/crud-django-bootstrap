38d656f0eef66c27b584481f6c601e29d0047ef9
luciano-mesquita <lucianomesquita01@gmail.com>
2020-03-11 16:57:35 -0300

    Refactoring templates and implementing logi

--- app/urls.py
+++ app/urls.py
@@ -6,9 +6,4 @@ urlpatterns = [
     path('persons-create/', views.persons_create, name='persons_create'),
     path('persons-update/<int:id>', views.persons_update, name='persons_update'),
     path('persons-delete/<int:id>', views.persons_delete, name='persons_delete'),
-
-    path('users-list/', views.users_list, name='users_list'),
-    path('users-create/', views.users_create, name='users_create'),
-    path('users-update/<int:id>/', views.users_update, name='users_update'),
-    path('users-delete/<int:id>', views.users_delete, name='users_delete'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'home',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [
